c9693445aa3f1b024a325bcedff2fd096265f9d6
Dave Yarwood <dave.yarwood@gmail.com>
2023-01-22 19:42:57 -0500

    Start implementing `iterateGame`

--- src/game.ts
+++ src/game.ts
@@ -1,21 +1,65 @@
 import { standardDeck, Card } from "./deck";
 import { shuffle } from "./shuffle";
 
+interface Player {
+  cardsInHand: Card[];
+  cardsInPlay: Card[];
+}
+
+export enum GameStatus {
+  StillPlaying,
+  Player1Won,
+  Player2Won,
+}
+
 export interface Game {
-  player1: Card[];
-  player2: Card[];
+  player1: Player;
+  player2: Player;
+  status: GameStatus;
+}
+
+function initialPlayer(initialHand: Card[]): Player {
+  return {
+    cardsInHand: initialHand,
+    cardsInPlay: [],
+  };
 }
 
 export function initialGame(): Game {
   const deck = shuffle(standardDeck());
 
   return {
-    player1: deck.slice(0, 27),
-    player2: deck.slice(27, 54),
+    status: GameStatus.StillPlaying,
+    player1: initialPlayer(deck.slice(0, 27)),
+    player2: initialPlayer(deck.slice(27, 54)),
   };
 }
 
 export function iterateGame(game: Game): Game {
+  if (game.status != GameStatus.StillPlaying) {
+    return game;
+  }
+
+  if (game.player1.cardsInHand.length == 0) {
+    game.status = GameStatus.Player2Won;
+    return game;
+  }
+
+  if (game.player2.cardsInHand.length == 0) {
+    game.status = GameStatus.Player1Won;
+    return game;
+  }
+
+  if (game.player1.cardsInPlay.length == 0) {
+    const card1: Card = game.player1.cardsInHand.shift()!;
+    game.player1.cardsInPlay.push(card1);
+
+    const card2: Card = game.player2.cardsInHand.shift()!;
+    game.player2.cardsInPlay.push(card2);
+
+    return game;
+  }
+
   // TODO:
   // * if one player is out of cards, then the other player wins
   // * get a card from player1 deck and player2 deck
---
+++ test/game.test.ts
@@ -0,0 +1,62 @@
+import { expect, test } from "vitest";
+import { Card, standardDeck } from "../src/deck";
+import { GameStatus, initialGame, iterateGame } from "../src/game";
+import { shuffle } from "../src/shuffle";
+
+test("Initial game", () => {
+  const newGame = initialGame();
+
+  expect(newGame.status).toEqual(GameStatus.StillPlaying);
+
+  expect(newGame.player1.cardsInHand.length).toEqual(27);
+  expect(newGame.player1.cardsInPlay.length).toEqual(0);
+  expect(newGame.player2.cardsInHand.length).toEqual(27);
+  expect(newGame.player2.cardsInPlay.length).toEqual(0);
+});
+
+function randomCards(amount: number): Card[] {
+  return shuffle(standardDeck()).slice(0, amount);
+}
+
+test("Win/lose checking", () => {
+  // Note: This is slightly unrealistic in that the game wouldn't be in this
+  // state without the status already _being_ Player2Won. But it's a good basic
+  // check of the win/lose checking logic.
+  const game1 = iterateGame({
+    status: GameStatus.StillPlaying,
+    player1: { cardsInHand: [], cardsInPlay: [] },
+    player2: { cardsInHand: randomCards(54), cardsInPlay: [] },
+  });
+
+  expect(game1.status).toEqual(GameStatus.Player2Won);
+  expect(iterateGame(game1).status).toEqual(GameStatus.Player2Won);
+
+  const game2 = iterateGame({
+    status: GameStatus.StillPlaying,
+    player1: { cardsInHand: randomCards(54), cardsInPlay: [] },
+    player2: { cardsInHand: [], cardsInPlay: [] },
+  });
+
+  expect(game2.status).toEqual(GameStatus.Player1Won);
+  expect(iterateGame(game2).status).toEqual(GameStatus.Player1Won);
+
+  const game3 = iterateGame({
+    status: GameStatus.StillPlaying,
+    player1: { cardsInHand: randomCards(27), cardsInPlay: [] },
+    player2: { cardsInHand: randomCards(27), cardsInPlay: [] },
+  });
+
+  expect(game3.status).toEqual(GameStatus.StillPlaying);
+});
+
+test("First iteration", () => {
+  const newGame = initialGame();
+  const gameAfterIteration = iterateGame(newGame);
+
+  expect(newGame.status).toEqual(GameStatus.StillPlaying);
+
+  expect(gameAfterIteration.player1.cardsInHand.length).toEqual(26);
+  expect(gameAfterIteration.player1.cardsInPlay.length).toEqual(1);
+  expect(gameAfterIteration.player2.cardsInHand.length).toEqual(26);
+  expect(gameAfterIteration.player2.cardsInPlay.length).toEqual(1);
+});
