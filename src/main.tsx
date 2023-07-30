8345cd221046eb755c875674fa21b844fd30e7da
Dave Yarwood <dave.yarwood@gmail.com>
2023-07-30 16:24:32 -0400

    Prettier formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // Removed because it was causing the timers to be set twice on page load.
   //
   // Reference: https://stackoverflow.com/q/60618844/2338327
@@ -10,4 +10,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
   <App />
   // </React.StrictMode>,
-)
+);
